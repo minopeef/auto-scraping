1ed751a78db714398b124be11ee14b8fde114b45
tomokomori11 <tomokomori1111@gmail.com>
2023-03-12 15:56:19 +0100

    add yakiusoku engine

--- scrap.py
+++ scrap.py
@@ -12,6 +12,7 @@ class Base:
         self.name = None
         self.path = None
         self.url = None
+        self.driver_id = None
 
     def download_img(self, path: str, link: str):
         r = requests.get(link, stream=True)
@@ -53,13 +54,14 @@ class Whatjpride(Base):
         self.name = "なんJ PRIDE"
         self.path = "自動化/なんJ PRIDE"
         self.url = "http://blog.livedoor.jp/rock1963roll/"
+        self.driver_id = "1uOXxuaaQA4dgX8puRF49-Mbzdb21Eonp"
 
     def run(self):
         resp = requests.get(self.url)
         soup = BeautifulSoup(resp.text, features="html.parser")
-        ul_tag = soup.find("ul", attrs={"class": "recent-article-image"})
-        link_list = [x.find("a")["href"] for x in ul_tag.find_all("li")]
-        for _link in link_list:
+        recent_tag = soup.find("ul", attrs={"class": "recent-article-image"})
+        recent_link_list = [x.find("a")["href"] for x in recent_tag.find_all("li")]
+        for _link in recent_link_list:
             result = {
                 "title": "",
                 "link": _link,
@@ -128,6 +130,88 @@ class Whatjpride(Base):
         return
 
 
-Whatjpride().run()
+class Yakiusoku(Base):
+    def __init__(self) -> None:
+        self.name = "日刊やきう速報"
+        self.path = "自動化/日刊やきう速報"
+        self.url = "http://blog.livedoor.jp/yakiusoku/"
+
+    def run(self):
+        resp = requests.get(self.url)
+        soup = BeautifulSoup(resp.text, features="html.parser")
+        recent_tag = soup.find("ul", attrs={"class": "recent-article-image"})
+        recent_link_list = [x.find("a")["href"] for x in recent_tag.find_all("li")]
+        for _link in recent_link_list:
+            result = {
+                "title": "",
+                "link": _link,
+                "path": "",
+                "comment": [],
+                "img_link": "",
+            }
+            # get all html
+            resp = requests.get(_link)
+            soup = BeautifulSoup(resp.text, features="html.parser")
+
+            # get article head html
+            article_head = soup.find(attrs={"class": "article-header"})
+
+            # get deployed time
+            date_time = article_head.find(attrs={"class": "article-date"}).text.strip()
+            date_time = "".join(re.findall(r"\d+", date_time))[:12]
+
+            # get title
+            title = article_head.find(attrs={"class": "article-title"}).text.strip()
+            result["title"] = title
+
+            # get save path
+            article_head = " ".join(re.findall(r"\w*", title)).strip()
+            path = f"{self.path}/{date_time}_{article_head}"
+            result["path"] = path
+
+            # get article body html
+            article_body = soup.find("div", attrs={"class": "article-body-inner"})
+
+            # get image link
+            img_link = article_body.find_all("img")[0]["src"]
+            result["img_link"] = img_link
+
+            # create path
+            Path(f"{path}/音声ファイル").mkdir(parents=True, exist_ok=True)
+
+            # save image
+            self.download_img(path, img_link)
+
+            # get comment head and body
+            comment_head_list = [
+                x.text.strip()
+                for x in article_body.find_all("div", attrs={"class": "t_h"})
+            ]
+            comment_body_list = [
+                x.text.strip()
+                for x in article_body.find_all("div", attrs={"class": "t_b"})
+            ]
+
+            # download audio and comment
+            for idx, item in enumerate(comment_head_list):
+                result["comment"].append({"head": item, "body": comment_body_list[idx]})
+
+                temp_arr = re.findall(r"\w+", item)
+                file_name = str(idx) + "_" + temp_arr[1] + temp_arr[-1]
+
+                self.save_audio(
+                    f"{path}/音声ファイル/{file_name}.mp3", comment_body_list[idx]
+                )
+
+                self.save_txt(f"{path}/{file_name}.txt", comment_body_list[idx])
+            with open(f"{path}/all_info.json", mode="w", encoding="utf-8") as f:
+                json.dump(result, f, ensure_ascii=False)
+
+        return
+
+
+
+# Whatjpride().run()
+Yakiusoku().run()
 # Base().download_audio("static","""abc""")
 # print(os.listdir("自動化/MLB NEWS@まとめ"))
